46be09566abbd7bbb8739fecd12dec65f9e7c139
Mark Jason Espelita <mjespelita.swashed@gmail.com>
2023-09-21 11:53:23 +0800

    asdsa

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer class="footer-wrapper per-section" id="Contact">
+        <div class="container-hf">
+            <div class="right-f">
+                <img src="/images/footerR.png" alt="">
+            </div>
+            <div class="left-f">
+                <h2 class="title-ov">CONTACT</h2>
+                <div class="links-f">
+                    <p class="link">
+                        <span>
+                            <img src="/images/Group 241.png" alt="">
+                        </span>
+                        <a class="link" href="tel:+63 975 644 6261">+63 975 644 6261</a>
+                    </p>
+                    <p class="link">
+                        <span>
+                            <img src="/images/Icon material-email.png" alt="">
+                        </span>
+                        gvisionld@gmail.com
+                    </p>
+                    <p class="link">
+                        <span>
+                            <img src="/images/Group 242.png" alt="">
+                        </span>
+                        <a class="link" href="https://wa.me/09756446261">WhatsApp +63 975 644 6261</a><br/>
+
+                    </p>
+                    <a class="link send-msg-btn" href="mailto:gvisionld@gmail.com?subject=Hi GVisionLD&body=message%20goes%20here" target="_blank">
+                        SEND MESSAGE
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+</template>
